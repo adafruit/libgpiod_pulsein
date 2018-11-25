ebabe5a4536ecf51c18b90de82a9e3c1cdd26f05
ladyada <limor@ladyada.net>
2018-11-25 02:19:28 -0500

    add a circular buffer and sysv signalling

--- src/libgpiod_pulsein.c
+++ src/libgpiod_pulsein.c
@@ -1,31 +1,40 @@
-#include <gpiod.h>
-#include "libgpiod_pulsein.h"
-
 #include <getopt.h>
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/time.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <sys/msg.h>
 #include <sched.h>
 #include <signal.h>
+#include <gpiod.h>
+#include "libgpiod_pulsein.h"
+#include "circular_buffer.h"
+
+#define VMSG_MAXSIZE 4096
+struct vmsgbuf {
+  long msg_type;
+  char message[VMSG_MAXSIZE];
+};
 
 
-int pulse_count = 0;
-unsigned int pulses[MAX_PULSES] = {0};
+unsigned int pulses[MAX_PULSE_BUFFER] = {0};
+cbuf_handle_t ringbuffer;
+volatile bool paused = false;
 
 const char *consumername = "libgpiod_pulsein";
 
 static const struct option longopts[] = {
 	{ "help",	no_argument,	NULL,	'h' },
 	{ "version",	no_argument,	NULL,	'v' },
-	{ "active-low",	no_argument,	NULL,	'l' },
+	{ "idle_state",	no_argument,	NULL,	'i' },
 	{ "trigger",    required_argument,      NULL,   'd' },
 	{ "pulses",	required_argument,	NULL,	'p' },
 	{ "timeout",	required_argument,	NULL,	't' },
+	{ "queue",      required_argument,      NULL,   'q' },
 };
 
-static const char *const shortopts = "+hvlptd";
+static const char *const shortopts = "+hviptd";
 
 static void print_help(void)
 {
@@ -35,10 +44,11 @@ static void print_help(void)
 	printf("Options:\n");
 	printf("  -h, --help:\t\tdisplay this message and exit\n");
 	printf("  -v, --version:\tdisplay the version and exit\n");
-	printf("  -l, --active-low:\tset the line active state to low\n");
-	printf("  -p, --pulses:\tnumber of state changes to record before exit\n");
+	printf("  -i, --idle_state:\tset the line idle state to HIGH (defalt is low)\n");
+	printf("  -p, --pulses:\tnumber of pulses to store in ring buffer\n");
 	printf("  -t, --timeout:\tnumber microseconds to wait before exit\n");
 	printf("  -d, --trigger:\tSend an initial output pulse of n microseconds\n");
+	printf("  -q, --queue:\tID number of SYSV queue for IPC\n");
 }
 
 
@@ -46,11 +56,10 @@ static void print_help(void)
 
 int main(int argc, char **argv) {
 	int offset, optc, opti, 
-	  value, previous_value, 
-	  wanted_pulses = 0;
+	  value, previous_value;
+	int max_pulses = MAX_PULSE_BUFFER;
 	int32_t timeout_microseconds = 0, trigger_len_us = 0;
-	bool active_low = false, 
-	  count_pulses = false, 
+	bool idle_state = false,
 	  exit_on_timeout = false,
 	  trigger_pulse = false,
 	  fast_linux = false;
@@ -64,6 +73,8 @@ int main(int argc, char **argv) {
 	double previous_time, current_time;
 	long int previous_tick, current_tick;
 	float us_per_tick = 0;
+	struct vmsgbuf vmbuf;
+	int queue_id = 0, queue_key = 0;
 
 	for (;;) {
 		optc = getopt_long(argc, argv, shortopts, longopts, &opti);
@@ -77,14 +88,13 @@ int main(int argc, char **argv) {
 		case 'v':
 			printf("libgpiod_pulsein v0.0.1\n");
 			return EXIT_SUCCESS;
-		case 'l':
-			active_low = true;
+		case 'i':
+			idle_state = true;
 			break;
 		case 'p':
-		        count_pulses = true;
-			wanted_pulses = strtoul(optarg, &end, 10);
+			max_pulses = strtoul(optarg, &end, 10);
 			if (*end != '\0' || offset > INT_MAX) {
-				printf("invalid pulse count: %s", optarg);
+				printf("invalid max pulse count: %s", optarg);
 				exit(1);
 			}
 			break;
@@ -104,7 +114,13 @@ int main(int argc, char **argv) {
 				exit(1);
 			}
 			break;
-
+		case 'q':
+			queue_key = strtoul(optarg, &end, 10);
+			if (*end != '\0' || offset > INT_MAX) {
+				printf("invalid queue key: %s", optarg);
+				exit(1);
+			}
+			break;
 		default:
 			abort();
 		}
@@ -137,10 +153,9 @@ int main(int argc, char **argv) {
 	  exit(1);
 	}
 
-
-	// Bump up process priority and change scheduler to try to try to make process more 'real time'.
+	// Bump up process priority and change scheduler to try  
+	// to make process more 'real time'.
 	set_max_priority();
-
 
 	chip = gpiod_chip_open_by_name(device);
 	if (!chip) {
@@ -153,35 +168,26 @@ int main(int argc, char **argv) {
 	  exit(1);
 	}
 
-	if (!fast_linux && us_per_tick == 0) {
-	  // self calibrate best we can
-	  //printf("Calculating us per tick\n");
-
-	  if (gpiod_line_request_input(line, consumername) != 0) {
-	    printf("Unable to set line %d to input\n", offset);
+	// Set up message passing system, if requested
+	if (queue_key != 0) {
+	  queue_id = msgget(queue_key, IPC_CREAT);
+	  if (queue_id == -1) {
+	    printf("Unable to create message queue\n");
 	    exit(1);
 	  }
-
-	  gettimeofday(&time_event, NULL);
-	  previous_time = time_event.tv_sec;
-	  previous_time *= 1000000;
-	  previous_time += time_event.tv_usec;
-
-	  for (int i=0; i<100; i++) {
-	    int ret = gpiod_line_get_value(line);
-	    if (ret == -1) {
-	      printf("Unable to read line %d\n", offset);
-	      exit(1);
-	    }
+	  memset(vmbuf.message, 0, VMSG_MAXSIZE);
+	  while (msgrcv(queue_id, (struct msgbuf *)&vmbuf, 
+			VMSG_MAXSIZE, 1, IPC_NOWAIT) != -1) {
+	    // flush by reading every message
 	  }
-	  gettimeofday(&time_event, NULL);
-	  current_time = time_event.tv_sec;
-	  current_time *= 1000000;
-	  current_time += time_event.tv_usec;
-	  us_per_tick = (current_time - previous_time) / 100;
-	  //printf("us_per_tick: %f\n", us_per_tick);
-	  // Be kind, rewind!
-	  gpiod_line_release(line);
+	  // tell them we're ready!
+	  vmbuf.message[0] = '!';
+	  vmbuf.msg_type = 2;
+	  msgsnd(queue_id, (struct msgbuf *)&vmbuf, 1, 0);
+	}
+
+	if (!fast_linux && us_per_tick == 0) {
+	  us_per_tick = calculate_us_per_tick(line);
 	}
 
 #if defined(FOLLOW_PULSE)
@@ -198,35 +204,14 @@ int main(int argc, char **argv) {
 	}
 #endif
 
-	if (trigger_pulse) {
-	  //printf("Triggering output for %d microseconds\n", trigger_len_us);
-	  // set to an output
-	  if (gpiod_line_request_output(line, consumername, active_low) != 0) {
-	    printf("Unable to set line %d to output\n", offset);
-	    exit(1);
-	  }
-	  // set 'high'
-	  if (gpiod_line_set_value(line, !active_low) != 0) {
-	    printf("Unable to set line %d to active level\n", offset);
-	    exit(1);
-	  }
-	  // wait
-	  usleep(trigger_len_us);
-	  // set 'low'
-	  if (gpiod_line_set_value(line, active_low) != 0) {
-	    printf("Unable to set line %d to active level\n", offset);
-	    exit(1);
-	  }
-
-	  // release for input usage
-	  gpiod_line_release(line);
-	}
-
-
 	// set to an input
 	if (gpiod_line_request_input(line, consumername) != 0) {
 	  printf("Unable to set line %d to input\n", offset);
 	  exit(1);
+	}
+
+	if (trigger_pulse) {
+	  pulse_output(line, idle_state, trigger_len_us);
 	}
 
 	if ( fast_linux) {
@@ -238,14 +223,58 @@ int main(int argc, char **argv) {
 	  previous_tick = current_tick = 0;
 	}
 
+	// a simple ring buffer
+	ringbuffer = circular_buf_init(pulses, max_pulses);
+	circular_buf_reset(ringbuffer);
 
-	previous_value = gpiod_line_get_value(line);
-	if (previous_value == -1) {
-	  printf("Unable to read line %d\n", offset);
-	  exit(1);
-	}
+	// We record the first change from the idle_state
+	previous_value = idle_state;
 
 	for (;;) {
+	  if (queue_key != 0) {
+	    vmbuf.msg_type = 1;
+	    int msglen = msgrcv(queue_id, (struct msgbuf *)&vmbuf, 
+				VMSG_MAXSIZE, 1, IPC_NOWAIT);
+	    if ((msglen != -1) && (msglen >= 1)) {
+	      vmbuf.message[msglen] = 0;
+	      //printf("got %d byte message: %s\n", msglen, vmbuf.message);
+	      char cmd = vmbuf.message[0];
+	      if (cmd == 'p') {
+		// pause
+		paused = true;
+	      } else if (cmd == 'r') {
+		// resume
+		paused = false;
+	      } else if (cmd == 'c') {
+		// clear
+		circular_buf_reset(ringbuffer);
+	      } else if (cmd == 'l') {
+		// send back length
+		int buflen = circular_buf_size(ringbuffer);
+		snprintf(vmbuf.message, 15, "%d", buflen);
+		vmbuf.msg_type = 2;
+		msgsnd(queue_id, (struct msgbuf *)&vmbuf, strlen(vmbuf.message), 0);
+	      } else if (cmd == 't') {
+		// Resume with trigger pulse!
+		unsigned int trigger_len = strtoul(vmbuf.message+1, NULL, 10);
+		//printf("trigger %d\n", trigger_len);
+		pulse_output(line, idle_state, trigger_len);
+		paused = false;
+	      } else if (cmd == '^') {
+		// pop one message off and send it
+		unsigned int pulse;
+		if (circular_buf_get(ringbuffer, &pulse) == -1) {
+		  pulse = -1;
+		}
+		snprintf(vmbuf.message, 15, "%d", pulse);
+		vmbuf.msg_type = 2;
+		msgsnd(queue_id, (struct msgbuf *)&vmbuf, strlen(vmbuf.message), 0);
+	      }
+	    } 
+	  }
+	  if (paused) {
+	    continue;
+	  }
 	  value = gpiod_line_get_value(line);
 	  if (value < 0) {
 	    printf("Unable to read line %d\n", offset);
@@ -283,19 +312,8 @@ int main(int argc, char **argv) {
 	    }
 #endif
 	  if (value != previous_value) {
-	    pulses[pulse_count] = delta;
-	    pulse_count++;
-	    //printf("%d\t%0.f\n", value, delta);
-	      
-	    // If the user asked us to limit returned state changes, keep track
-	    // and exit when the count is satisfied:
-	    if (count_pulses) {
-	      if (pulse_count == wanted_pulses) {
-		printf("pulsed out\n");
-		print_pulses();
-		return EXIT_SUCCESS;
-	      }
-	    }
+	    circular_buf_put(ringbuffer, delta);
+
 	    previous_value = value;
 	    if ( fast_linux) {
 	      previous_time = current_time;
@@ -310,19 +328,29 @@ int main(int argc, char **argv) {
 }
 
 
-void sig_handler(int signo)
-{
+void sig_handler(int signo) {
   if (signo == SIGINT) {
-    printf("received SIGINT\n");
+    fprintf(stderr, "received SIGINT\n");
+    print_pulses();
+    exit(0);
   }
-  print_pulses();
-  exit(0);
+  if (signo == SIGTSTP) {
+    fprintf(stderr, "pausing\n");
+    paused = true;
+  }
+  if (signo ==  SIGCONT) {
+    fprintf(stderr, "un-pausing\n");
+    paused = false;
+  }
 }
 
 void print_pulses(void) {
-  //printf("%d PULSES\n", pulse_count);
+  int pulse_count = circular_buf_size(ringbuffer);
   for (int i=0; i<pulse_count; i++) {
-    printf("%d", pulses[i]);
+    unsigned int pulse = 0;
+    circular_buf_get(ringbuffer, &pulse);
+    
+    printf("%d", pulse);
     if (i != pulse_count-1) {
       printf(", ");
     }
@@ -337,4 +365,71 @@ void set_max_priority(void) {
   // Use FIFO scheduler with highest priority for the lowest chance of the kernel context switching.
   sched.sched_priority = sched_get_priority_max(SCHED_FIFO);
   sched_setscheduler(0, SCHED_FIFO, &sched);
+}
+
+
+void pulse_output(struct gpiod_line *line, bool idle_state, int trigger_len_us) {
+  //printf("Triggering output for %d microseconds\n", trigger_len_us);
+  gpiod_line_release(line);
+  // set to an output
+  if (gpiod_line_request_output(line, consumername, !idle_state) != 0) {
+    printf("Unable to set line to output\n");
+    exit(1);
+  }
+  // set 'high'
+  if (gpiod_line_set_value(line, idle_state) != 0) {
+    printf("Unable to set line for trigger pulse\n");
+    exit(1);
+  }
+  // wait
+  usleep(trigger_len_us);
+  // set 'low'
+  if (gpiod_line_set_value(line, !idle_state) != 0) {
+    printf("Unable to set line for trigger pulse\n");
+    exit(1);
+  }
+
+  // release for input usage
+  gpiod_line_release(line);
+
+  // set back to an input
+  if (gpiod_line_request_input(line, consumername) != 0) {
+    printf("Unable to set line to input\n");
+    exit(1);
+  }
+}
+
+
+float calculate_us_per_tick(struct gpiod_line *line) {
+  struct timeval time_event;
+  double previous_time, current_time;
+  // self calibrate best we can
+  //printf("Calculating us per tick\n");
+
+  if (gpiod_line_request_input(line, consumername) != 0) {
+    printf("Unable to set line to input\n");
+    exit(1);
+  }
+
+  gettimeofday(&time_event, NULL);
+  previous_time = time_event.tv_sec;
+  previous_time *= 1000000;
+  previous_time += time_event.tv_usec;
+
+  for (int i=0; i<100; i++) {
+    int ret = gpiod_line_get_value(line);
+    if (ret == -1) {
+      printf("Unable to read line during calibration\n");
+      exit(1);
+    }
+  }
+  gettimeofday(&time_event, NULL);
+  current_time = time_event.tv_sec;
+  current_time *= 1000000;
+  current_time += time_event.tv_usec;
+  float us_per_tick = (current_time - previous_time) / 100;
+  //printf("us_per_tick: %f\n", us_per_tick);
+  // Be kind, rewind!
+  gpiod_line_release(line);
+  return us_per_tick;
 }
--- src/libgpiod_pulsein.h
+++ src/libgpiod_pulsein.h
@@ -1,7 +1,10 @@
 
 //#define FOLLOW_PULSE  19
-#define MAX_PULSES 1000
+#define MAX_PULSE_BUFFER 1000
+
 
 void set_max_priority(void);
 void sig_handler(int signo);
 void print_pulses(void);
+float calculate_us_per_tick(struct gpiod_line *line);
+void pulse_output(struct gpiod_line *line, bool idle_state, int trigger_len_us);
